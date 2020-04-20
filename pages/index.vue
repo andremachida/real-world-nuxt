51478e26b9def490305bf6a9d0d48715b8c861ae
Andre Machida <andmachida@gmail.com>
2020-04-19 23:34:50 -0300

    Initial commit;

---
+++ pages/index.vue
@@ -0,0 +1,41 @@
+<template>
+  <div>
+    <h1>Events</h1>
+    <event-card
+      v-for="(event, index) in events"
+      :key="index"
+      :event="event"
+      :data-index="index"
+    />
+  </div>
+</template>
+
+<script>
+import { mapState } from 'vuex'
+import EventCard from '@/components/EventCard.vue'
+
+export default {
+  components: {
+    EventCard
+  },
+  async fetch({ store, error }) {
+    try {
+      await store.dispatch('events/fetchEvents')
+    } catch (err) {
+      error({ statusCode: 503, message: err.message })
+    }
+  },
+  computed: {
+    ...mapState({
+      events: (state) => state.events.events
+    })
+  },
+  head() {
+    return {
+      title: 'Event Listing'
+    }
+  }
+}
+</script>
+
+<style lang="scss" scoped></style>
